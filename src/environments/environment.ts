72ca1b01e4a499023021de21c22b592784d9e527
Adrian Werimo <adrwer7@gmail.com>
2021-10-13 16:32:00 +0300

    firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBIJd7v2aOwk-5R9xvxSznc6wmk7mVbDXQ",
+    authDomain: "kanbanfire-b6381.firebaseapp.com",
+    projectId: "kanbanfire-b6381",
+    storageBucket: "kanbanfire-b6381.appspot.com",
+    messagingSenderId: "754149599991",
+    appId: "1:754149599991:web:5de007902cb2b0298b81f2"
+  }
 };
 
 /*
