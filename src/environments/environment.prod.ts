43c04a972f965915fa7bea8cc6b64fecb3250f10
Adrian Werimo <adrwer7@gmail.com>
2021-10-14 17:34:42 +0300

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBIJd7v2aOwk-5R9xvxSznc6wmk7mVbDXQ",
+    authDomain: "kanbanfire-b6381.firebaseapp.com",
+    projectId: "kanbanfire-b6381",
+    storageBucket: "kanbanfire-b6381.appspot.com",
+    messagingSenderId: "754149599991",
+    appId: "1:754149599991:web:5de007902cb2b0298b81f2"
+  }
 };
